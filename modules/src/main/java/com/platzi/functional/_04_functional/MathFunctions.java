7ab65614b6c9f8d6c18d5a3bc55bf037e103ccb5
Jesús Rivera Flores <Jesus.RiveraF98@gmail.com>
2021-07-07 16:31:51 -0500

    Class 12: MathFunctios - Predicate

--- modules/src/main/java/com/platzi/functional/_04_functional/MathFunctions.java
+++ modules/src/main/java/com/platzi/functional/_04_functional/MathFunctions.java
@@ -14,5 +14,28 @@ public class MathFunctions {
                 };
         System.out.println(squareFunction.apply(5));
         System.out.println(squareFunction.apply(25));
+
+        Function<Integer, Boolean> isOdd = x -> x % 2 == 1;
+
+        Predicate<Integer> isEven = x -> x % 2 == 0;
+
+        isEven.test(4); //true
+
+        Predicate<Student> isApproved = student -> student.getgrade() >= 6.0;
+
+        Student sinuhe = new Student(5.9);
+        System.out.println(isApproved.test(sinuhe));
+    }
+
+    static class Student {
+        private double grade;
+
+        public Student(double grade) {
+            this.grade = grade;
+        }
+
+        public double getgrade() {
+            return grade;
+        }
     }
 }
